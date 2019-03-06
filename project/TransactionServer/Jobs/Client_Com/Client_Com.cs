44f48bf7e238de724580353d9c3b123774e5d661
gaofenghua <gaofenghua@gmail.com>
2019-03-06 09:53:08 +0800

    add Client_Com module

--- project/TransactionServer/Jobs/Client_Com/Client_Com.cs
+++ project/TransactionServer/Jobs/Client_Com/Client_Com.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using socket.framework.Server;
 
 namespace TransactionServer.Jobs.Client_Com
 {
     class Client_Com : Base.ServiceJob
     {
+        public CC_SocketServer SocketServer = null;
         protected override void Init()
         {
             // throw new NotImplementedException();
@@ -47,7 +49,64 @@ namespace TransactionServer.Jobs.Client_Com
 
         public void executeLogic()
         {
+            SocketServer = new CC_SocketServer(10, 1024, 0, 12345, 0xFF);
+        }
+    }
+
+    public class CC_SocketServer
+    {
+        TcpPackServer server;
+        /// <summary>
+        /// 设置基本配置
+        /// </summary>   
+        /// <param name="numConnections">同时处理的最大连接数</param>
+        /// <param name="receiveBufferSize">用于每个套接字I/O操作的缓冲区大小(接收端)</param>
+        /// <param name="overtime">超时时长,单位秒.(每10秒检查一次)，当值为0时，不设置超时</param>
+        /// <param name="port">端口</param>
+        /// <param name="headerFlag">包头</param>
+        public CC_SocketServer(int numConnections, int receiveBufferSize, int overtime, int port, uint headerFlag)
+        {
+            server = new TcpPackServer(numConnections, receiveBufferSize, overtime, headerFlag);
+            server.OnAccept += Server_OnAccept;
+            server.OnReceive += Server_OnReceive;
+            server.OnSend += Server_OnSend;
+            server.OnClose += Server_OnClose;
+            server.OnDisconnect += Server_OnDisconnect;
+            server.Start(port);
         }
 
+        private void Server_OnAccept(int obj)
+        {
+            //server.SetAttached(obj, 555);
+            Console.WriteLine($"Pack已连接{obj}");
+
+        }
+
+        private void Server_OnSend(int arg1, int arg2)
+        {
+            //Console.WriteLine($"Pack已发送:{arg1} 长度:{arg2}");
+        }
+
+        private void Server_OnReceive(int arg1, byte[] arg2)
+        {
+            //int aaa = server.GetAttached<int>(arg1);
+            //Console.WriteLine($"Pack已接收:{arg1} 长度:{arg2.Length}");          
+            server.Send(arg1, arg2, 0, arg2.Length);
+
+
+
+        }
+
+        private void Server_OnClose(int obj)
+        {
+            //int aaa = server.GetAttached<int>(obj);
+            Console.WriteLine($"Pack断开{obj}");
+        }
+
+        private void Server_OnDisconnect(int obj)
+        {
+            //int aaa = server.GetAttached<int>(obj);
+            Console.WriteLine($"Pack中断{obj}");
+        }
     }
 }

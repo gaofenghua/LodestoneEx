a05ce2f11e891c61583881e3d961019c8a080a37
Irene Wang <irenew@axis.com>
2019-03-11 11:49:13 +0800

    1. copy dll for Bosch 2. remove debug info

--- project/TransactionServer/Service1.cs
+++ project/TransactionServer/Service1.cs
@@ -49,11 +49,7 @@ namespace TransactionServer
         }
 
         protected override void OnStart(string[] args)
-        {
-            // debug
-            Thread.Sleep(1000 * 30);
-            //
-            this.runJobs();
+        {            this.runJobs();
         }
 
         protected override void OnStop()
@@ -84,9 +80,9 @@ namespace TransactionServer
                             }
                             string assemblyName = sectionType.Split(',')[1];
                             string classFullName = string.Empty;
-                            if (2 == sectionName.Split('_').Length & false) //This conflict need to be fixed -- Indy
+                            if (2 == sectionName.Split('-').Length)
                             {
-                                classFullName = assemblyName + ".Jobs." + sectionName.Split('_')[0] + ".Config";
+                                classFullName = assemblyName + ".Jobs." + sectionName.Split('-')[0] + ".Config";
                             }
                             else
                             {
